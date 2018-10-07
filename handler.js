519a79d8e020d5c70a09687f3274cc5cce5e20d1
Ben Hadfield <benjohnhadfield@gmail.com>
2018-10-07 13:24:10 +0100

    load env from yml file

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 module.exports.ping = async (event, context) => {
     return {
         statusCode: 200,
-        body: JSON.stringify({ exit: 0 })
+        body: JSON.stringify({ status: 0 })
     }
 }
